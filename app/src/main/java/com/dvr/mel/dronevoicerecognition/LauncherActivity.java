80aa842d22fe46aae12418a6cb8890afa493fce8
Evan <evan.graine@gmail.com>
2016-11-13 19:02:16 +0100

    Interface Tests

---
+++ app/src/main/java/com/dvr/mel/dronevoicerecognition/LauncherActivity.java
@@ -0,0 +1,32 @@
+package com.dvr.mel.dronevoicerecognition;
+
+import android.app.Activity;
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+public class LauncherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //Remove title bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        //Remove notification bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        //Set content view
+        setContentView(R.layout.activity_launcher);
+
+        //Custom font
+        TextView tx = (TextView)findViewById(R.id.launcherTitle);
+        Typeface custom_font = Typeface.createFromAsset(getAssets(),  "fonts/letraHipster.ttf");
+        tx.setTypeface(custom_font);
+
+    }
+}
