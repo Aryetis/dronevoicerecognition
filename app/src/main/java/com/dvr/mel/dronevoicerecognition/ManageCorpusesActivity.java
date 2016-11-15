b9609391749d45092107dc8ed158f8a791920b49
Evan <evan.graine@gmail.com>
2016-11-15 23:58:26 +0100

    Can add and remove items in list

--- app/src/main/java/com/dvr/mel/dronevoicerecognition/ManageCorpusesActivity.java
+++ app/src/main/java/com/dvr/mel/dronevoicerecognition/ManageCorpusesActivity.java
@@ -1,14 +1,20 @@
 package com.dvr.mel.dronevoicerecognition;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,6 +26,8 @@ public class ManageCorpusesActivity extends AppCompatActivity {
     RecyclerView.Adapter adapter;
     RecyclerView recyclerView;
     Context context = this;
+    // TODO Link with real data
+    final ArrayList<String> mockList = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,8 +36,7 @@ public class ManageCorpusesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_manage_corpuses);
         if(getSupportActionBar() != null) getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        // TODO Link with real data
-        final ArrayList<String> mockList = new ArrayList<String>();
+
         // Layout manager of recycler view
         LinearLayoutManager layoutManager = new LinearLayoutManager(context);
 
@@ -55,21 +62,18 @@ public class ManageCorpusesActivity extends AppCompatActivity {
                 final int finalpos = position;
                 ((RoundedLetterView)holder.itemView.findViewById(R.id.rlv_rlv)).setTitleText("C"+position);
                 ((TextView)holder.itemView.findViewById(R.id.rlv_text_view)).setText(mockList.get(position));
-                holder.itemView.setOnClickListener(new View.OnClickListener() {
+                holder.itemView.setOnCreateContextMenuListener(new View.OnCreateContextMenuListener() {
                     @Override
-                    public void onClick(View view) {
-                        // Todo Mocked
-                        Toast.makeText(context, "You Clicked "+mockList.get(finalpos), Toast.LENGTH_LONG).show();
+                    public void onCreateContextMenu(ContextMenu contextMenu, final View view, ContextMenu.ContextMenuInfo contextMenuInfo) {
+
+                        contextMenu.setHeaderTitle("Select an action");
+                        contextMenu.add(0, finalpos, 0, "Set as reference");
+                        contextMenu.add(0, finalpos, 0, "Remove");
+                        ManageCorpusesActivity.super.onCreateContextMenu(contextMenu, view, contextMenuInfo);
                     }
+
                 });
-                holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
-                    @Override
-                    public boolean onLongClick(View view) {
-                        // Todo Mocked
-                        Toast.makeText(context, "You Long Clicked "+mockList.get(finalpos), Toast.LENGTH_LONG).show();
-                        return false;
-                    }
-                });
+
             }
 
             @Override
@@ -77,6 +81,7 @@ public class ManageCorpusesActivity extends AppCompatActivity {
                 return mockList.size();
             }
         };
+
 
         // Get recycler view
         recyclerView = (RecyclerView) findViewById(R.id.corpuses_recyclerview);
@@ -95,13 +100,53 @@ public class ManageCorpusesActivity extends AppCompatActivity {
         // Add layout Manager
         recyclerView.setLayoutManager(layoutManager);
 
+    }
 
-        mockList.add("Corpus de Référence");
-        mockList.add("Corpus Matthias");
-        mockList.add("Corpus Leo");
-        mockList.add("Corpus Evan");
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        switch (item.getTitle().toString()){
+            case "Remove":
+                mockList.remove(item.getItemId());
+                adapter.notifyDataSetChanged();
+                break;
+            case "Set as reference":
+                break;
+        }
+        Toast.makeText(context, "Removed " + item.getItemId(), Toast.LENGTH_SHORT).show();
+        return super.onContextItemSelected(item);
+    }
 
-        adapter.notifyDataSetChanged();
+    public void addNewCorpus(View view) {
 
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = this.getLayoutInflater();
+
+        builder.setTitle("New Corpus");
+
+        builder.setView(View.inflate(inflater.getContext(), R.layout.new_corpus_form, null));
+
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                EditText nameEditText =
+                        (EditText)((AlertDialog)dialog).findViewById(R.id.newCorpusNameEditText);
+                String name = (nameEditText != null ? nameEditText.getText().toString().trim() : "");
+                if (!name.isEmpty()) {
+                    mockList.add(name);
+                    adapter.notifyDataSetChanged();
+                }
+                else
+                    Toast.makeText(context, "The name field is empty!", Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User cancelled the dialog
+                dialog.cancel();
+            }
+        });
+
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
