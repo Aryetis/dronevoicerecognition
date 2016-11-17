08dfa87ed7fcf2bd600b9b9ce11eb0958442df90
leo <leocances@gmail.com>
2016-11-17 15:45:20 +0100

    populateCorpusInfo methode implemented (not tested yet)
    All fields add in

--- app/src/main/java/com/dvr/mel/dronevoicerecognition/CorpusInfo.java
+++ app/src/main/java/com/dvr/mel/dronevoicerecognition/CorpusInfo.java
@@ -1,14 +1,38 @@
 package com.dvr.mel.dronevoicerecognition;
 
 import java.io.File;
+import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by leo on 16/11/16.
  */
 
-public class CorpusInfo {
+public class CorpusInfo implements Serializable{
+    public static File baseDir, corpusGlobalDir;
+    public static File wordTestingDir;
     public static List<String> referencesCorpora = new ArrayList<>();
-    public static File corpusGlobalDir;
+    public static List<String> usersCorpora = new ArrayList<>();
+    public static List<String> commands = new ArrayList<>();
+    public static Map<String, Corpus> corpusMap = new HashMap<>();
+
+    public File _baseDir, _corpusGlobalDir;
+    public File _wordTestingDir;
+    public List<String> _referencesCorpora = new ArrayList<>();
+    public List<String> _usersCorpora = new ArrayList<>();
+    public List<String> _commands = new ArrayList<>();
+    public Map<String, Corpus> _corpusMap = new HashMap<>();
+
+
+    public CorpusInfo() {
+        _baseDir = baseDir;
+        _corpusGlobalDir = corpusGlobalDir;
+        _wordTestingDir = wordTestingDir;
+        _referencesCorpora = referencesCorpora;
+        _usersCorpora = usersCorpora;
+        _commands = commands;
+    }
 }
