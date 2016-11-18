99ed310878bec8ed297101f718f5e4e582051844
leo <leocances@gmail.com>
2016-11-19 00:02:32 +0100

    suppression d'une fonction obsolette

--- app/src/main/cpp/reconnaissanceVocale.h
+++ app/src/main/cpp/reconnaissanceVocale.h
@@ -7,10 +7,12 @@
 
 #define CORPUS_DIRECTORY    "./corpus/dronevolant_bruite"
 
+std::vector<std::string> vocabulaires = { "avance", "recule", "droite", "gauche",
+                                          "etatdurgence", "tournedroite", "tournegauche",
+                                          "faisunflip", "arretetoi" };
 
 Matrix<float> parametrisation(std::string word);
 std::string buildPath(std::string init, std::string locuteur, std::string word);
-std::string reconnaissance(std::string unknowWord);
 
 
 
